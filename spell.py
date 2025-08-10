17f31ba97c0081ba5c389ee1a53937230d0bbec7
Gourab Shil <shilgourab7@gmail.com>
2025-08-10 12:46:17 +0530

    Add files via upload

---
+++ spell.py
@@ -0,0 +1,35 @@
+from spellchecker import SpellChecker
+
+class spellcheckerApp():
+    def __init__(self):
+        self.spell = SpellChecker()
+
+
+    def correct_text(self, text):
+        words = text.split()
+        corrected_words = []
+
+        for word in words:
+            corrected_word = self.spell.correction(word)
+            if corrected_word != word.lower():
+                print(f"Correcting {word} to {corrected_word}")
+                corrected_words.append(corrected_word)
+
+    # returning the corrected text
+
+        return ''.join(corrected_word)
+    
+    def run(self):
+        print("\n ==== Spell Checker ==== ")
+        while True:
+            text = input("Enter text to check or type 'exit to quit: ")
+            if text.lower() == 'exit':
+                print("Closing the program..")
+                break
+            
+            corrected_text = self.correct_text(text)
+            print(f"Corrected Text : {corrected_text}")
+            
+if __name__ == "__main__":
+    spellcheckerApp().run()
+
